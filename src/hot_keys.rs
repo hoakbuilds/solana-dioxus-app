d12717f93c684c2c714577d2753d29dfc6d743f1
Eliascm17 <Elias.cmoreno17@gmail.com>
2023-05-03 13:16:03 -0500

    keep only needed hot keys

--- src/hot_keys.rs
+++ src/hot_keys.rs
@@ -31,59 +31,63 @@ pub fn HotKeys(cx: Scope) -> Element {
                         w_search_state.active = !w_search_state.active;
                         w_search_state.query = "".to_string();
                     }
-                    "G" | "g" => {
-                        goto_mode = true;
+                    "Escape" => {
+                        let mut w_search_state = search_state.write();
+                        w_search_state.active = false;
                     }
-                    "A" | "a" => {
-                        if goto_mode {
-                            router.navigate_to("/accounts");
-                            goto_mode = false;
-                        }
-                    }
-                    "F" | "f" => {
-                        if goto_mode {
-                            router.navigate_to("/files");
-                            goto_mode = false;
-                        }
-                    }
+                    // "G" | "g" => {
+                    //     goto_mode = true;
+                    // }
+                    // "A" | "a" => {
+                    //     if goto_mode {
+                    //         router.navigate_to("/accounts");
+                    //         goto_mode = false;
+                    //     }
+                    // }
+                    // "F" | "f" => {
+                    //     if goto_mode {
+                    //         router.navigate_to("/files");
+                    //         goto_mode = false;
+                    //     }
+                    // }
                     "H" | "h" => {
                         if goto_mode {
                             router.navigate_to("/");
                             goto_mode = false;
                         }
                     }
-                    "P" | "p" => {
-                        if goto_mode {
-                            router.navigate_to("/programs");
-                            goto_mode = false;
-                        }
-                    }
-                    "K" | "k" => {
-                        if goto_mode {
-                            router.navigate_to("/keys");
-                            goto_mode = false;
-                        }
-                    }
-                    "J" | "j" => {
-                        goto_mode = false;
-                        let id = list_index.map_or(0, |i| i + 1);
-                        let elem_id = format!("list-item-{}", id);
-                        if let Some(element) = document.get_element_by_id(&*elem_id) {
-                            if element.unchecked_into::<HtmlElement>().focus().is_ok() {
-                                list_index = Some(id);
-                            }
-                        }
-                    }
-                    "K" | "k" => {
-                        goto_mode = false;
-                        let id = list_index.map_or(0, |i| i.saturating_sub(1));
-                        let elem_id = format!("list-item-{}", id);
-                        if let Some(element) = document.get_element_by_id(&*elem_id) {
-                            if element.unchecked_into::<HtmlElement>().focus().is_ok() {
-                                list_index = Some(id);
-                            }
-                        }
-                    }
+                    // "P" | "p" => {
+                    //     if goto_mode {
+                    //         router.navigate_to("/programs");
+                    //         goto_mode = false;
+                    //     }
+                    // }
+                    // "K" | "k" => {
+                    //     if goto_mode {
+                    //         router.navigate_to("/keys");
+                    //         goto_mode = false;
+                    //     }
+                    // }
+                    // "J" | "j" => {
+                    //     goto_mode = false;
+                    //     let id = list_index.map_or(0, |i| i + 1);
+                    //     let elem_id = format!("list-item-{}", id);
+                    //     if let Some(element) = document.get_element_by_id(&*elem_id) {
+                    //         if element.unchecked_into::<HtmlElement>().focus().is_ok() {
+                    //             list_index = Some(id);
+                    //         }
+                    //     }
+                    // }
+                    // "K" | "k" => {
+                    //     goto_mode = false;
+                    //     let id = list_index.map_or(0, |i| i.saturating_sub(1));
+                    //     let elem_id = format!("list-item-{}", id);
+                    //     if let Some(element) = document.get_element_by_id(&*elem_id) {
+                    //         if element.unchecked_into::<HtmlElement>().focus().is_ok() {
+                    //             list_index = Some(id);
+                    //         }
+                    //     }
+                    // }
                     _ => {}
                 }
             }))
